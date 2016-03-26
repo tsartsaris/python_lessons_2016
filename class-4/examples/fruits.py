19ea3e98c8cbcb975f0e2ddd263e2fcd5ede9317
mixasgr <mixasgr@greeklug.gr>
2016-03-26 19:31:18 +0200

    Update fruits.py

--- class-4/examples/fruits.py
+++ class-4/examples/fruits.py
@@ -1,1 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  fruit.py
+#  
+#  Copyright 2016 Greeklug Team <greeklug.gr>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
 
+l_fr = ['banana', 'axladi', 'milo', 'karpouzi', 'peponi']
+l_pr = [1.00, 1.25, 2.10, 5.00, 3.45]
+
+def friutCost(l1,l2,c): 
+
+    l3=dict(zip(l1, l2))
+    #print l3
+    
+    counter = 0
+    totalcost = 0
+    
+    for fruitcost in l3.itervalues():
+		#print fruitcost
+		totalcost = totalcost + fruitcost
+		counter = counter + 1
+		if counter >=c:
+			print "##################"
+			print "ΕΦΑΡΜΟΓΗ ΑΘΡΟΙΣΜΑΤΟΣ ΤΙΜΩΝ ΦΡΟΥΤΩΝ"
+			print "##################"
+			print "Φρούτα που προστέθηκαν:"
+			print c
+			print "----------------"
+			print "Αποτέλεσμα σε Ευρώ:"
+			print totalcost
+			print "##################"
+			print "\n"
+			break
+
+friutCost(l_fr,l_pr,3)
